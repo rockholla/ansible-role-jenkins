36907c289a0abefede39f98d653de0283239ed38
Patrick Force <patrickforce@gmail.com>
2017-04-04 11:22:14 -0600

    update log message when creating local user

--- templates/basic-security.groovy
+++ templates/basic-security.groovy
@@ -7,7 +7,7 @@ def instance = Jenkins.getInstance()
 println "--> Checking if security has been set already"
 
 if (!instance.isUseSecurity()) {
-    println "--> creating local user 'admin'"
+    println "--> creating local user '{{ jenkins_admin_username }}'"
 
     def hudsonRealm = new HudsonPrivateSecurityRealm(false)
     hudsonRealm.createAccount('{{ jenkins_admin_username }}', '{{ jenkins_admin_password }}')
